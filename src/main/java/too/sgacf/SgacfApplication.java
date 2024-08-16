6cd5d4d4b09240d850ea29d2fd287b2de4148338
KathCardenas <kath9704.gmail.com>
2024-08-16 09:48:19 -0600

    Initial commit

---
+++ src/main/java/too/sgacf/SgacfApplication.java
@@ -0,0 +1,13 @@
+package too.sgacf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgacfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgacfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/too/sgacf/SgacfApplicationTests.java
@@ -0,0 +1,13 @@
+package too.sgacf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgacfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
